74e11e00d6282bbd99e8ab250a4d1e526ef6bcce
Michael Baxter <meb.michael@gmail.com>
2016-06-05 00:38:00 -0700

    Code 10.1

--- InOneWeekendSwift/camera.swift
+++ InOneWeekendSwift/camera.swift
@@ -7,10 +7,15 @@ final class camera {
     final let horizontal: vec3
     final let vertical: vec3
 
-    init() {
-        lowerLeftCorner = vec3(-2, -1, -1)
-        horizontal = vec3(4, 0, 0)
-        vertical = vec3(0, 2, 0)
+    init(vFov: Float, aspect: Float) {
+        
+        let theta = vFov * Float(M_PI) / 180
+        let halfHeight = tan(theta / 2)
+        let halfWidth = aspect * halfHeight
+        
+        lowerLeftCorner = vec3(-halfWidth, -halfHeight, -1)
+        horizontal = vec3(2 * halfWidth, 0, 0)
+        vertical = vec3(0, 2 * halfHeight, 0)
         origin = vec3(0, 0, 0)
     }
 
