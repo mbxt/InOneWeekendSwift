87a920a159eb947e3a75486f48c3c76fb43486ad
Michael Baxter <meb.michael@gmail.com>
2016-06-04 20:06:16 -0700

    Code 5.2

---
+++ InOneWeekendSwift/hitable.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+struct hitRecord {
+    
+    let t: Float
+    let p: vec3
+    let normal: vec3
+    
+    init(t: Float, p: vec3, normal: vec3) {
+        self.t = t
+        self.p = p
+        self.normal = normal
+    }
+}
+
+/*
+ * We return the `hitRecord` as an optional, rather than modify an instance passed by reference.
+ * As a result, `hitRecord` is returned as a constant struct, which is actually more performant
+ * in Swift than continually passing a single `hitRecord` by reference, as in the book's C++ code.
+ *
+ * Additionally, it should be noted that at least in this case, inheritance is more performant than
+ * protocol conformance (i.e., using a class versus a protocol for the interface of the `hit` method).
+ */
+class hitable {
+    func hit(r: ray, tMin: Float, tMax: Float) -> hitRecord? {
+        return nil
+    }
+}
