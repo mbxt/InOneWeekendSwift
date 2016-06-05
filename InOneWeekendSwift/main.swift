be90480fe9d127e67a93ec5d9852f7ab2cb4027f
Michael Baxter <meb.michael@gmail.com>
2016-06-04 18:53:00 -0700

    Code 2.5

--- InOneWeekendSwift/main.swift
+++ InOneWeekendSwift/main.swift
@@ -8,13 +8,11 @@ print("P3\n\(nx) \(ny)\n255\n")
 for j in (0..<ny).reverse() {
     for i in 0..<nx {
         
-        let r = Float(i) / Float(nx)
-        let g = Float(j) / Float(ny)
-        let b = 0.2
+        let col = vec3(Float(i) / Float(nx), Float(j) / Float(ny), 0.2)
         
-        let ir = Int(255.99 * r)
-        let ig = Int(255.99 * g)
-        let ib = Int(255.99 * b)
+        let ir = Int(255.99 * col.r)
+        let ig = Int(255.99 * col.g)
+        let ib = Int(255.99 * col.b)
         
         print("\(ir) \(ig) \(ib)")
     }
